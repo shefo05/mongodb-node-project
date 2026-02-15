4e20dbf5a7295d11794ff3398c7f82f74d15079e
shefo05 <shafeelgendy@gmail.com>
2026-02-15 03:01:27 +0200

    add the bonus

---
+++ bonus/bonus.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+var romanToInt = function (s) {
+  const romanValues = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let result = 0;
+  let prevValue = 0;
+
+  for (let i = s.length - 1; i >= 0; i--) {
+    const currValue = romanValues[s[i]];
+    if (currValue >= prevValue) {
+      result += currValue;
+    } else {
+      result -= currValue;
+    }
+    prevValue = currValue;
+  }
+
+  return result;
+};
